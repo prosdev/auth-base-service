24d48975c254cbb799a875bc1b137aa79b544575
pseng <pros.seng@healthsparq.com>
2017-05-15 16:53:16 -0700

    bcrypt password

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -4,6 +4,10 @@ exports.signup = function(req, res, next) {
     const email = req.body.email;
     const password = req.body.password;
 
+    if(!email || !password) {
+        return res.status(422).send({ error: 'Must provide email and password.'})
+    }
+
     //See if a user with the given email exists
     User.findOne({ email: email }, function (err, existingUser) {
         if (err) { return next(err); }
@@ -22,7 +26,7 @@ exports.signup = function(req, res, next) {
             if (err) { return next(err); }
 
             //Respond to request indicating the user was created
-            res.json(user);
+            res.json({ success: true });
         });
 
     })
--- models/user.js
+++ models/user.js
@@ -1,12 +1,31 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-
+const bcrypt = require('bcrypt-nodejs');
 //Define our model
 const userSchema = new Schema({
     email: { type: String, unique: true, lowercase: true },
     password: String
 });
 
+//onSave hook, encrypt password
+//Before saving model, run encypt function
+userSchema.pre('save', function(next) {
+    const user = this;
+
+    //generate salt
+    bcrypt.genSalt(10, function(err, salt) {
+        if (err) { return next(err); }
+
+        //encrypt password
+        bcrypt.hash(user.password, salt, null, function(err, hash) {
+           if (err) { return next(err); }
+
+           //override plain-text with encrypted password
+           user.password = hash;
+           next();
+        });
+    })
+});
 //Create the model class
 const UserModel = mongoose.model('user', userSchema);
 
