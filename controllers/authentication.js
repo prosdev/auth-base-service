f7808b8cd9bb7a655d91b02a276654bc540556dd
pseng <pros.seng@healthsparq.com>
2017-05-15 17:05:19 -0700

    Implemented jwt for auth

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,4 +1,12 @@
 const User = require('../models/user');
+const jwt = require('jwt-simple');
+const config = require('../config');
+
+function tokenForUser(user) {
+    const timestamp = new Date().getTime();
+    return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
+}
+
 
 exports.signup = function(req, res, next) {
     const email = req.body.email;
@@ -26,7 +34,7 @@ exports.signup = function(req, res, next) {
             if (err) { return next(err); }
 
             //Respond to request indicating the user was created
-            res.json({ success: true });
+            res.json({ token: tokenForUser(user) });
         });
 
     })
