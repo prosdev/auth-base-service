f44c767b57ffba578e954be6f5b8dd6a58068d0d
pseng <pros.seng@healthsparq.com>
2017-05-15 17:41:36 -0700

    Implement local strategy for sign in

--- services/passport.js
+++ services/passport.js
@@ -1,8 +1,33 @@
 const passport = require('passport');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
+const LocalStrategy = require('passport-local');
 const User = require('../models/user');
 const config = require('../config');
+
+//Create Local strategy
+const localOptions = { usernameField: 'email'};
+const localLogin = new LocalStrategy(localOptions, function (email, password, done) {
+    //Verify this email and password, call done with user
+    //otherwise call done with false
+    User.findOne({ email: email }, function (err, user) {
+        if (err) { return done(err); }
+
+        if(!user) {
+            return done(null, false);
+        }
+
+        //Compare passwords
+        user.comparePassword(password, function (err, isMatch) {
+            if (err) { return done(err); }
+            if(!isMatch) {
+                return done(null, false);
+            }
+
+            return done(null, user);
+        })
+    });
+});
 
 //Set up options for JWT Strategies
 const jwtOptions = {
@@ -28,3 +53,4 @@ const jwtLogin = new JwtStrategy(jwtOptions, function (payload, done) {
 
 //Tell Passport to use strategy
 passport.use(jwtLogin);
+passport.use(localLogin);
